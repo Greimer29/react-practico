4ee15469cdeff8b9cce3f5cc5432acc925017cc0
Greimer29 <greimerabelguzman29@gmail.com>
2021-05-02 14:48:35 -0400

    Aja

--- myfirtsapp/src/components/components.js
+++ myfirtsapp/src/components/components.js
@@ -1,3 +1,4 @@
+import { users } from '../db.json';
 import React, { Component } from 'react';
 import '../App.css';
 import './sidebar.components.scss';
@@ -7,13 +8,11 @@ class Options extends Component {
         return (
             <div className="option">
                 <div className="container">
-                    <a href="/tickets" className="link">
+                    <a href={this.props.link} className="link">
                         <div className="option-img text-center">
                         </div>
-                        <div className="option-description ms-2">
-                            {
-                                this.props.imagen
-                            }
+                        <div className="option-description">
+                            <h6>{this.props.description}</h6>
                         </div>
                     </a>
                 </div>
@@ -23,19 +22,29 @@ class Options extends Component {
 }
 
 class Sidebar extends Component {
+    constructor() {
+        super();
+        this.state = {
+            users
+        }
+    }
     render() {
+        const user = this.state.users[1]
         return (
             <div id="sidebar" className="sidebar">
                 <div className="user">
                     <div className="user-option-img text-center">
                         <i className="bi bi-person"></i>
                     </div>
+                    <div className="user-description">
+                        <h5>{user.name}</h5>
+                    </div>
                 </div>
                 <hr />
                 <div className="container">
-                    <Options imagen="Tickets" />
-                    <Options imagen="Billetera" />
-                    <Options imagen="Ajustes" />
+                    <Options link='/tickets' description='Tickets' />
+                    <Options link='/billetera' description="Billetera" />
+                    <Options link='/ajustes' description="Ajustes" />
                 </div>
             </div >
         )
