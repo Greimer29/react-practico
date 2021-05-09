bde98bc228188188dfc6dfba3a43016440fbd90a
Greimer29 <greimerabelguzman29@gmail.com>
2021-05-09 13:26:45 -0400

    Changes into the sidebar components

--- src/components/components.js
+++ src/components/components.js
@@ -3,23 +3,23 @@ import React, { Component } from 'react';
 import '../App.css';
 import './sidebar.components.scss';
 
-class Options extends Component {
-    render() {
-        return (
-            <div className="option">
-                <div className="container">
-                    <a href={this.props.link} className="link">
-                        <div className="option-img text-center">
-                        </div>
-                        <div className="option-description">
-                            <h6>{this.props.description}</h6>
-                        </div>
-                    </a>
-                </div>
-            </div>
-        )
-    }
-}
+// class Options extends Component {
+//     render() {
+//         return (
+//             <div className="option">
+//                 <div className="container">
+//                     <a href={this.props.link} className="link">
+//                         <div className="option-img text-center">
+//                         </div>
+//                         <div className="option-description">
+//                             <h6>{this.props.description}</h6>
+//                         </div>
+//                     </a>
+//                 </div>
+//             </div>
+//         )
+//     }
+// }
 
 class Sidebar extends Component {
     constructor() {
@@ -42,11 +42,41 @@ class Sidebar extends Component {
                 </div>
                 <hr />
                 <div className="container">
-                    <Options link='/tickets' description='Tickets' />
-                    <Options link='/billetera' description="Billetera" />
-                    <Options link='/ajustes' description="Ajustes" />
+                    <div className="option">
+                        <div className="container">
+                            <a href="/tickets" className="link">
+                                <div className="option-img text-center">
+                                </div>
+                                <div className="option-description">
+                                    <h6>Tickets <span className="badge badge-dark" >{user.tickets}</span></h6>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div className="option">
+                        <div className="container">
+                            <a href="/billetera" className="link">
+                                <div className="option-img text-center">
+                                </div>
+                                <div className="option-description">
+                                    <h6>Billetera</h6>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div className="option">
+                        <div className="container">
+                            <a href="/ajustes" className="link">
+                                <div className="option-img text-center">
+                                </div>
+                                <div className="option-description">
+                                    <h6>Ajustes</h6>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
                 </div>
-            </div >
+            </div>
         )
     }
 }
