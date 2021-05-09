b564452b377c5b228b2dcf6332a2940e2361f6e3
Greimer29 <greimerabelguzman29@gmail.com>
2021-05-09 15:41:01 -0400

    Get out Sidebar component, you will be better

--- src/index.js
+++ src/index.js
@@ -3,13 +3,11 @@ import ReactDOM from "react-dom";
 
 import "./index.css";
 
-import Sidebar from './components/components';
 import App from "./App";
 import transition from './components/tansition';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Sidebar />
     <App />
   </React.StrictMode>,
   document.getElementById("root")
