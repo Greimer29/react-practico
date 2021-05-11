fdb1465666a08db73bcfcf05f51e577a81948b03
Greimer29 <greimerabelguzman29@gmail.com>
2021-05-10 22:35:03 -0400

    Changes into de App, insert all in a <Router/> tag for use my Routes of the Sidebar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Sidebar from './components/Sidebar';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+
+import InputTicket from './pages/InputTicket';
+import Wallet from './pages/Wallet';
+import Ajustes from './pages/Ajustes';
 
 function App() {
   return (
     <div className="App" >
-    <Sidebar />
-      <header className="App-header" >
-        <img src={logo}
-          className="App-logo"
-          alt="logo" />
-        < p id="estado">
-          Edita < code > src / App.js </code> and save to reload.
-        </p>
-        <a className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer" > Learn React </a>
-      </header>
+      <Router>
+        <Sidebar />
+          <header className="App-header" >
+            <img src={logo}
+              className="App-logo"
+              alt="logo" />
+              < p id="estado">
+              Edita < code > src / App.js </code> and save to reload.
+              </p>
+            <a className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"> 
+              Learn React 
+            </a>
+          </header>
+
+          <Route path="/billetera" exact={true} component={Wallet}/>
+          <Route path="/tickets" exact={true}component={InputTicket}/>
+          <Route path="/ajustes" exact={true} component={Ajustes}/>
+      </Router>
     </div>
   );
 }
