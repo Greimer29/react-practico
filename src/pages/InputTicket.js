cd0a8167a28f69fe938d833f87e9c457e2b02289
Greimer29 <greimerabelguzman29@gmail.com>
2021-05-12 16:19:21 -0400

    Trying to use functions in my routes, but i'll to change it to simple buttons

--- src/pages/InputTicket.js
+++ src/pages/InputTicket.js
@@ -1,8 +1,41 @@
-const nose = ()=>{
-    return (
-        <div className="container">
-            <h1>Hello</h1>
+import React,{Component} from 'react';
+
+class nose extends Component{
+    constructor(){
+        super();
+        this.state={
+            tickets:100
+        }
+        this.handleInput = this.handleInput.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleInput(e){
+
+    }
+    handleSubmit(e){
+        e.preventDefault()
+        
+    }
+    render(){
+        return (
+        <div className="col-md-5">
+        <div className="card m-3">
+            <div className="card-header"><h3>Ingrese nro de Billetera <span className="badge badge-pill badge-danger bg-danger">{this.state.tickets}</span></h3></div>
+                <form className="card-body" onSubmit={this.handleSubmit}>
+                    <div className="form-group">
+                        <input 
+                            type="text"
+                            name="tickets"  
+                            className="form-control"
+                            placeholder="5" 
+                            onChange={this.handleInput} 
+                        />
+                    </div>
+                    <button type="submit"className="btn btn-primary">Guardar</button>
+                </form>
         </div>
+    </div>
     )
+}
 }
 export default nose;
