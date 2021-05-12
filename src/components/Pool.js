5c70d51d7f4fc85b5967559e95b79a87b4fc4f99
Greimer29 <greimerabelguzman29@gmail.com>
2021-05-12 00:19:46 -0400

    Advance into the Pool card styles

--- src/components/Pool.js
+++ src/components/Pool.js
@@ -1,24 +1,30 @@
 import React, {Component} from 'react';
-import logo from "../logo.svg";
+//;
 import './pool.scss';
 
 class Pool extends Component{
+    timer(){
+        let newTickets = window.prompt(`Mano dame los tickets perro`)
+        console.log(newTickets+5)
+    }
     render(){
         return(
             <div className="pool">
-                <div className="pool-container">
-                    <div className="pool-img">
-                        <img src={logo} alt="es un dai"/>
+                <div className="pool-clock"><h3>Time</h3></div>
+                    <div className="pool-container">
+                        <div className="pool-img">
+                        </div>
+                        <div className="publicidad">
+                            <p>Venga y gane mucho dinero aportando liquidez en un sorteo sin pérdidas</p>
+                            <h1 className="premio">1000 <span className="dai"></span></h1>
+                        <div className="thing"><p>yo bien</p></div>
                     </div>
-                    <div className="publicidad">
-                        <p>Venga y gane mucho dinero aportando liquidez en un sirteo sin pérdidas</p>
-                        <h1>1000 D</h1>
-                    <div className="thing">yo bien</div>
-                    </div>
-                    <button>Participar</button>
-                    <a href="/como">Cómo Funciona</a>
+                    <button className="btn btn-warning w-100" onClick={this.timer}>Participar</button>
+                    <a href="/como" className="link">Cómo Funciona</a>
                     <div className="pool-pozo">
-                        Personas que entraron
+                        <div className="container">
+                            Personas que entraron
+                        </div>
                     </div>
                 </div>
             </div>
