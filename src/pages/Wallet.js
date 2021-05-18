8ae7c838a141ef68c335c44c68f5ff625d97ee54
Greimer29 <greimerabelguzman29@gmail.com>
2021-05-18 19:14:58 -0400

    Change how looks the wallet form

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -19,26 +19,28 @@ class Wallet extends Component{
     }
 
     handleSubmit(e){
-        e.preventDefault();
-        alert(this.state.wallet)
+
     }
 
     render(){
         return(
-            <div className="col-md-5">
-                <div className="card m-3">
-                    <div className="card-header"><h3>Ingrese nro de Billetera <span className="badge badge-pill badge-danger bg-danger">{this.state.wallet}</span></h3></div>
-                        <form className="card-body" onSubmit={this.handleSubmit}>
+            <div className="col-md-5 container px-1">
+                <div className="card ">
+                    <div className="card-header text-dark">
+                        <h3>Ingrese nro de Billetera <span className="badge badge-danger bg-danger">{this.state.wallet}</span>
+                        </h3>
+                    </div>
+                        <form className="card-body" onSubmit={this.handleSubmit} action="/">
                             <div className="form-group">
                                 <input 
                                     type="text"
                                     name="wallet"  
-                                    className="form-control"
+                                    className="form-control mb-2"
                                     placeholder="Numero de Billetera" 
                                     onChange={this.handleInput} 
                                 />
                             </div>
-                            <button type="submit"className="btn-primary">Guardar</button>
+                            <button type="submit" className="btn btn-primary">Guardar</button>
                         </form>
                 </div>
             </div>
