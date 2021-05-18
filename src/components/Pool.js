1f0e41991548eaf6491b0fe421036333a84d37bc
Greimer29 <greimerabelguzman29@gmail.com>
2021-05-18 19:12:40 -0400

    Removing the fucking function that never works

--- src/components/Pool.js
+++ src/components/Pool.js
@@ -1,20 +1,11 @@
-import React, {Component} from 'react';
+import  React, {Component} from 'react';
 import './pool.scss';
 
 class Pool extends Component{
-    timer(){
-        let newTickets = parseInt(window.prompt(`Mano dame los tickets perro`))
-        const stock=100; 
-        if(newTickets<=100){
-            console.log(stock-newTickets)
-        }else{
-            console.log(`No tienes suficientes tickets`)
-        }
-    }
     render(){
         return(
             <div className="pool">
-                <div className="pool-clock"><h3>Time</h3></div>
+                <div id="pool-clock"className="pool-clock"><h3>Time</h3></div>
                     <div className="pool-container">
                         <div className="pool-img">
                         </div>
@@ -24,7 +15,7 @@ class Pool extends Component{
                             </p>
                             <h1 className="premio my-0">1.000 <span className="dai"></span></h1>
                         </div>
-                    <button className="btn btn-warning my-2 w-100" onClick={this.timer}>Participar</button>
+                    <button className="btn btn-warning my-2 w-100">Participar</button>
                         <a href="/como" className="link my-2">Cómo Funciona</a>
                     <div className="pool-pozo">
                         <div className="container">
