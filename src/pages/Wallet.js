956b682f317b72d3fa1ccdb0a51f030c00062913
Greimer29 <greimerabelguzman29@gmail.com>
2021-05-30 20:11:32 -0400

    Defining the functions who will get the form input

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -4,7 +4,7 @@ class Wallet extends Component{
     constructor(){
         super();
         this.state={
-            wallet:""
+            newWallet:""
         }
         this.handleInput = this.handleInput.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -15,11 +15,11 @@ class Wallet extends Component{
         this.setState({
             [name]:value
         })
-        console.log(this.state.wallet,value,name)
     }
 
     handleSubmit(e){
-
+        e.preventDefault()
+        console.log(this.state)
     }
 
     render(){
@@ -27,14 +27,14 @@ class Wallet extends Component{
             <div className="col-md-5 container px-1">
                 <div className="card ">
                     <div className="card-header text-dark">
-                        <h3>Ingrese nro de Billetera <span className="badge badge-danger bg-danger">{this.state.wallet}</span>
+                        <h3>Ingrese nro de Billetera <span className="badge badge-danger bg-danger">{this.state.newWallet}</span>
                         </h3>
                     </div>
-                        <form className="card-body" onSubmit={this.handleSubmit} action="/">
+                        <form className="card-body" onSubmit={this.handleSubmit}>
                             <div className="form-group">
                                 <input 
                                     type="text"
-                                    name="wallet"  
+                                    name="newWallet"  
                                     className="form-control mb-2"
                                     placeholder="Numero de Billetera" 
                                     onChange={this.handleInput} 
